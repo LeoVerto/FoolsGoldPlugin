37a21fceae982aadd5686a69db644e846c5fc413
legomaster181 <legomaster181@me.com>
2013-07-03 15:41:42 -0700

    Fixed server shutdown giving persistant slowness by resetting speed on login, not quit.

--- src/main/java/com/github/leoverto/foolsgoldplugin/PlayerJoinListener.java
+++ src/main/java/com/github/leoverto/foolsgoldplugin/PlayerJoinListener.java
@@ -3,20 +3,16 @@ package com.github.leoverto.foolsgoldplugin;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 
-public class PlayerQuitListener implements Listener {
+public class PlayerJoinListener implements Listener {
 	
 	@EventHandler
-	public void onPlayerQuit(PlayerQuitEvent event) {
+	public void onPlayerJoin(PlayerJoinEvent event) {
 		Player player = event.getPlayer();
 		String playerName = player.getName();
 		if (PlayerMoveListener.weGaveSlow.containsKey(playerName)) {
-			if (PlayerMoveListener.weGaveSlow.get(playerName)) {
-				PlayerMoveListener.weGaveSlow.remove(playerName);
-			} else {
-				PlayerMoveListener.weGaveSlow.remove(playerName);
-			}
+			PlayerMoveListener.weGaveSlow.remove(playerName);
 		}
 		player.setWalkSpeed(0.2f);
 	}
